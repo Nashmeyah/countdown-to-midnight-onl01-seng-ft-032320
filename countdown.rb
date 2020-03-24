a4f0cb3823d0f2778447b67e2588ff5204d0bc83
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-03-24 19:49:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,13 @@ def countdown(num)
     puts "HAPPY NEW YEAR!"
   end
 end
+
+
+def countdown_with_sleep(num)
+  while num > 0
+    puts "#{number} SECOND(S)!"
+    num -= 1
+    sleep 1
+    puts "HAPPY NEW YEAR!"
+  end
+end
